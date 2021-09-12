5eb9f7253dfc1824cb4d987e50009966ba828af8
gretten <any@anykey.com>
2021-09-12 20:31:54 +0300

    2 views

---
+++ src/components/LoginForm/LoginForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Логин:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.name"
+          placeholder="Введите логин"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Пароль:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          placeholder="Введите пароль"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary" class="mt-2" >Подтвердить</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          login: '',
+          password: '',
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        this.form.email = ''
+        this.form.name = ''
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
--- src/views/LoginPage/LoginPage.vue
+++ src/views/LoginPage/LoginPage.vue
@@ -1,10 +1,21 @@
 <template>
-    <div> helo!</div>
+    <b-row>
+        <b-col>
+            <div>
+                <LoginForm />
+            </div>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
+import LoginForm from '../../components/LoginForm/LoginForm.vue'
+
 export default {
     name: 'LoginPage',
+    components: {
+        LoginForm
+    }
 }
 </script>
 
