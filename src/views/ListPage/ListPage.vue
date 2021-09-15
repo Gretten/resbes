a90ff6e791349dae83d65186526e555ca8343105
gretten <any@anykey.com>
2021-09-15 11:48:54 +0300

    pagination bug fixed

--- src/views/ListPage/ListPage.vue
+++ src/views/ListPage/ListPage.vue
@@ -1,34 +1,53 @@
 <template>
 
-    <div v-if="!getListData.length" class="spinner-container d-flex align-items-center justify-content-center">
+    <div v-if="!objectsLength" class="spinner-container d-flex align-items-center justify-content-center">
          <Spinner />
     </div>
     <div v-else> 
+        <b-form-group
+          label="Поиск по имени"
+          label-for="filter-input"
+          label-cols-sm="3"
+          label-align-sm="right"
+          label-size="sm"
+          class="mb-0"
+        >
+          <b-input-group size="sm">
+            <b-form-input
+              id="filter-input"
+              v-model="filter"
+              type="search"
+              placeholder="Начните печатать для поиска..."
+            ></b-form-input>
+          </b-input-group>
+        </b-form-group>
+
         <b-table 
             striped 
             hover 
             id='listTable'
-            :items="getListData"
+            :items="objects"
             :fields="fields"
             :per-page="perPage"
             :current-page="currentPage"
+            :filter="filter"
+            :filter-included-fields="filterOn"
+            @filtered="onFiltered"
         ></b-table>
 
         <b-pagination
             v-model="currentPage"
-            :totalRows="rows"
+            :totalRows="maxRows"
             :perPage="perPage"
             ariaControls="listTable"
         ></b-pagination>
-
     </div>
 
-    
 </template>
 
 <script>
 import Spinner from '@/components/Spinner/Spinner'
-import { mapGetters } from 'vuex'
+import { mapState } from 'vuex';
 
 export default {
     name: 'ListPage',
@@ -48,25 +67,32 @@ export default {
                 ],
             perPage: 10,
             currentPage: 1,
+            maxRows: this.objectsLength,
+            filter: null,
+            filterOn: ['Name'],
         }
+    },    
+    mounted: function() {
+        this.maxRows = this.objectsLength;
     },
-    computed: {
-        ...mapGetters([
-            'getListData'
-        ]),
-        rows() {
-            return this.getListData.length
-        }
-    },
+    computed: mapState({
+        objects: state => state.listData,
+        objectsLength: state => state.listData.length
+    }),
     created: function() {
-        this.$store.dispatch('fetchListData')
+        this.$store.dispatch('fetchListData');
     },
     methods: {
         onFiltered(filteredItems) {
-            this.totalRows = filteredItems.length
-            this.currentPage = 1
+            this.maxRows = filteredItems.length;
+            this.currentPage = 1;
       },
-    }
+    },
+    watch: {
+        objects: function (val) {
+            this.maxRows = val.length;
+        }
+    },
 }
 </script>
 
